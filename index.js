dc905e8bb69e7883b38f20c71582942dad6d3175
Mickeyyadav22 <Mickeyyadav22@users.noreply.github.com>
2024-11-11 10:38:42 +0530

    this file is about to add events in dom to image

---
+++ index.js
@@ -0,0 +1,34 @@
+function showOpacity() {
+    let newfunction = document.getElementById("Opacity")
+    newfunction.innerHTML = "<img src='monster inc.jpg'style='Opacity:0.1' height='600px' width='400px'></img>"
+}
+function showOpacity1() {
+    let newfunction = document.getElementById("Opacity")
+    newfunction.innerHTML = "<img src='monster inc.jpg'style='Opacity:0.2' height='600px' width='400px'></img>"
+} function showOpacity2() {
+    let newfunction = document.getElementById("Opacity")
+    newfunction.innerHTML = "<img src='monster inc.jpg' style='Opacity:0.3' height='600px' width='400px'></img>"
+} function showOpacity3() {
+    let newfunction = document.getElementById("Opacity")
+    newfunction.innerHTML = "<img src='monster inc.jpg'style='Opacity:0.4' height='600px' width='400px'></img>"
+}
+function showOpacity4() {
+    let newfunction = document.getElementById("Opacity")
+    newfunction.innerHTML = "<img src='monster inc.jpg'style='Opacity:0.5' height='600px' width='400px'></img>"
+}
+function showOpacity5() {
+    let newfunction = document.getElementById("Opacity")
+    newfunction.innerHTML = "<img src='monster inc.jpg'style='Opacity:0.6' height='600px' width='400px'></img>"
+}
+function showOpacity6() {
+    let newfunction = document.getElementById("Opacity")
+    newfunction.innerHTML = "<img src='monster inc.jpg'style='Opacity:0.7' height='600px' width='400px'></img>"
+}
+function showOpacity7() {
+    let newfunction = document.getElementById("Opacity")
+    newfunction.innerHTML = "<img src='monster inc.jpg'style='Opacity:0.8' height='600px' width='400px'></img>"
+}
+function showOpacity8() {
+    let newfunction = document.getElementById("Opacity")
+    newfunction.innerHTML = "<img src='monster inc.jpg'style='Opacity:1' height='600px' width='400px'></img>"
+}
